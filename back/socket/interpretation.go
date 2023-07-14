9ecee88bd6de594165452f3bb626b4ac60aaf219
BetterCallMolly <molly@mana.rip>
2023-07-14 11:22:25 +0200

    update: more readable

--- back/socket/interpretation.go
+++ back/socket/interpretation.go
@@ -7,24 +7,30 @@ import (
 )
 
 // A function that does nothing, called if a unexpected packet is received
-func noop() error {
-	return nil
+func noop() (*GenericPacket, error) {
+	return nil, nil
 }
 
 var (
-	PacketHandlers = map[uint8]func() error{}
+	PacketHandlers = map[uint8]func() (*GenericPacket, error){}
 )
 
+type GenericPacket struct {
+	Type uint8
+	Data []byte
+}
+
 func init() {
+	// We're not supposed to receive these packets from the client
 	PacketHandlers[packets.CYA_PACKET_ID] = noop
 	PacketHandlers[packets.WELCOME_PACKET_ID] = noop
 }
 
-func InterpretPacket(data []byte) error {
+func InterpretPacket(data []byte) (*GenericPacket, error) {
 	packetType := data[0]
 	if handler, ok := PacketHandlers[packetType]; ok {
 		return handler()
 	} else {
-		return fmt.Errorf("unknown packet type: %d", packetType)
+		return nil, fmt.Errorf("unknown packet type: %d", packetType)
 	}
 }
