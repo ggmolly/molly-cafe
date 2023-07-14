2abe70738724f025ed1eb1e836dd5db2c429c991
BetterCallMolly <molly@mana.rip>
2023-07-14 14:01:44 +0200

    fix: ids according to the documentation

--- back/socket/packets/ids.go
+++ back/socket/packets/ids.go
@@ -1,7 +1,7 @@
 package packets
 
 const (
-	WELCOME_PACKET_ID = uint8(0b10000000)
-	CYA_PACKET_ID     = uint8(0b10000001)
-	MOUSE_MOVE_ID     = uint8(0b01000000)
+	WELCOME_PACKET_ID = uint8(0b01000000)
+	CYA_PACKET_ID     = uint8(0b01000001)
+	MOUSE_MOVE_ID     = uint8(0b01000010)
 )
