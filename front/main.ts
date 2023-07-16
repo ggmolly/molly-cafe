d68ddba983bb156bdf9dd862366470b3bc457273
BetterCallMolly <molly@mana.rip>
2023-07-16 13:32:50 +0200

    Revert "update: new ideas"
    This reverts commit 32ddca233082e08fb9ca1533f1695fa9833e4a1d.

--- front/main.ts
+++ front/main.ts
@@ -108,12 +108,81 @@ const MOUSE_MOVE_ID = 0b01000010;
 const CLIENT_PACKET_TYPE = 0b01000000;
 let socket = new Socket();
 
-function getSelectedSprite() {
-    const select = document.getElementById("selected-sprite") as HTMLSelectElement;
-    console.log("Selected sprite : ", select.options[select.selectedIndex].value)
-    return select.options[select.selectedIndex].value;
-}
-
 document.addEventListener("readystatechange", async function (event) {
     if (document.readyState !== "complete") return;
+
+    let swindle: Swindle = new Swindle();
+    let tiles = Array<Tile>();
+
+    const default_grass = await swindle.loadSprite("default_grass", "assets/grasses/grass_0.png");
+    const top_border = await swindle.loadSprite("top_border", "assets/borders/top_border.png");
+    const bottom_border = await swindle.loadSprite("bottom_border", "assets/borders/bottom_border.png");
+    const left_border = await swindle.loadSprite("left_border", "assets/borders/left_border.png");
+    const right_border = await swindle.loadSprite("right_border", "assets/borders/right_border.png");
+    const top_left_corner = await swindle.loadSprite("top_left_corner", "assets/borders/top_left_border.png");
+    const top_right_corner = await swindle.loadSprite("top_right_corner", "assets/borders/top_right_border.png");
+    const bottom_left_corner = await swindle.loadSprite("bottom_left_corner", "assets/borders/bottom_left_border.png");
+    const bottom_right_corner = await swindle.loadSprite("bottom_right_corner", "assets/borders/bottom_right_border.png");
+    const small_sapling = await swindle.loadSprite("small_sapling", "assets/objects/plants_3.png");
+
+    swindle.init(document.getElementById("canvas") as HTMLCanvasElement);
+
+    for (let y = 0; y < 3; y++) {
+        for (let x = 0; x < 3; x++) {
+            if (x === 0 && y === 0) {
+                tiles.push(new Tile(top_left_corner));
+                continue ;
+            }
+            if (x === 0 && y === 2) {
+                tiles.push(new Tile(bottom_left_corner));
+                continue ;
+            }
+            if (x === 2 && y === 0) {
+                tiles.push(new Tile(top_right_corner));
+                continue ;
+            }
+            if (x === 2 && y === 2) {
+                tiles.push(new Tile(bottom_right_corner));
+                continue ;
+            }
+            if (x > 0 && x < 2 && y === 0) {
+                tiles.push(new Tile(top_border));
+                continue ;
+            }
+            if (x > 0 && x < 2 && y === 2) {
+                tiles.push(new Tile(bottom_border));
+                continue ;
+            }
+            if (x === 0 && y > 0 && y < 2) {
+                tiles.push(new Tile(left_border));
+                continue ;
+            }
+            if (x === 2 && y > 0 && y < 2) {
+                tiles.push(new Tile(right_border));
+                continue ;
+            }
+            tiles.push(new Tile(default_grass));
+        }
+    }
+
+    const emptyTile = new Tile();
+    let tileMap: Tilemap = new Tilemap(3, 3, tiles);
+    let layers: Array<Layer> = new Array<Layer>();
+
+    // To prove that layers are working, we're gonna add a small sapling at 1, 1
+    let saplingTileMapTiles = Array<Tile>();
+    saplingTileMapTiles.push(emptyTile);
+    saplingTileMapTiles.push(new Tile(small_sapling));
+    saplingTileMapTiles.push(emptyTile);
+    saplingTileMapTiles.push(emptyTile);
+    saplingTileMapTiles.push(emptyTile);
+    saplingTileMapTiles.push(emptyTile);
+    saplingTileMapTiles.push(emptyTile);
+    saplingTileMapTiles.push(emptyTile);
+    saplingTileMapTiles.push(emptyTile);
+    let saplingTileMap: Tilemap = new Tilemap(3, 3, saplingTileMapTiles);
+    let saplingLayer: Layer = new Layer(3, 3, saplingTileMap);
+    layers.push(new Layer(3, 3, tileMap));
+    layers.push(saplingLayer);
+    swindle.render(layers);
 });
---
+++ front/swindle.ts
@@ -0,0 +1,185 @@
+/*
+    Swindle is a multi-pass 2D renderer. It is designed to be used with multiple tilemaps which are called layers.
+*/
+
+class Tile {
+    public readonly width: number;
+    public readonly height: number;
+    public readonly image?: HTMLImageElement = undefined;
+    public skip: boolean = false;
+    constructor(image?: HTMLImageElement, skip: boolean = false) {
+        if (image) {
+            this.width = image.width;
+            this.height = image.height;
+        } else {
+            this.skip = true;
+        }
+        this.image = image;
+    }
+}
+
+class Tilemap {
+    private tiles: Array<Tile>; // Flatten'd Matrix
+    public readonly width: number;
+    public readonly height: number;
+    constructor(width: number, height: number, preFillImage?: HTMLImageElement) {
+        this.width = width;
+        this.height = height;
+        this.tiles = new Array<Tile>(width * height);
+        // fill with empty tiles
+        if (!preFillImage) return;
+        for (let i = 0; i < this.tiles.length; i++) {
+            this.tiles[i] = new Tile(preFillImage);
+        }
+    }
+
+    public getTile(x: number, y: number): Tile {
+        if (x >= this.width || y >= this.height || x < 0 || y < 0) throw new Error("Tilemap index out of bounds.");
+        return this.tiles[y * this.width + x];
+    }
+
+    public setTile(x: number, y: number, tile: Tile) {
+        if (x >= this.width || y >= this.height || x < 0 || y < 0) throw new Error("Tilemap index out of bounds.");
+        const oldState = this.tiles[y * this.width + x];
+        this.tiles[y * this.width + x] = tile;
+    }
+
+    public removeTile(x: number, y: number) {
+        if (x >= this.width || y >= this.height || x < 0 || y < 0) throw new Error("Tilemap index out of bounds.");
+        this.tiles[y * this.width + x] = null;
+    }
+}
+
+/**
+ * A SwindleClickEvent is fired when the user clicks on the canvas.
+ * 
+ * @param x The x coordinate of the click.
+ * @param y The y coordinate of the click.
+ * @param tile The tile that was clicked on.
+ */
+class SwindleClickEvent {
+    public readonly x: number;
+    public readonly y: number;
+    public readonly tileX: number;
+    public readonly tileY: number;
+    public readonly tile: Tile;
+    constructor(x: number, y: number, tileX: number, tileY: number, tile: Tile) {
+        this.x = x;
+        this.y = y;
+        this.tileX = tileX;
+        this.tileY = tileY;
+        this.tile = tile;
+    }
+}
+
+/**
+ * A SwindleTileHoverEvent is fired when the user hovers over a tile.
+ * 
+ * @param x The x coordinate of the hover.
+ * @param y The y coordinate of the hover.
+ * @param tileX The x coordinate of the tile that was hovered over.
+ * @param tileY The y coordinate of the tile that was hovered over.
+ * @param tile The tile that was hovered over.
+ */
+class SwindleTileHoverEvent {
+    public readonly x: number;
+    public readonly y: number;
+    public readonly tileX: number;
+    public readonly tileY: number;
+    public readonly tile: Tile;
+    constructor(x: number, y: number, tileX: number, tileY: number, tile: Tile) {
+        this.x = x;
+        this.y = y;
+        this.tileX = tileX;
+        this.tileY = tileY;
+        this.tile = tile;
+    }
+}
+
+class Swindle {
+    private spriteMap: Map<string, HTMLImageElement> = new Map<string, HTMLImageElement>();
+    private canvas?: HTMLCanvasElement;
+    private ctx?: CanvasRenderingContext2D;
+    private tileWidth: number = 0;
+    private tileHeight: number = 0;
+    private fpsCounter?: HTMLElement;
+    private lastFrameTime: number = 0;
+    public tilemaps: Array<Tilemap> = new Array<Tilemap>();
+    public onSwindleClick: ((e: SwindleClickEvent) => void) | null = null;
+    public onSwindleTileHover: ((e: SwindleTileHoverEvent) => void) | null = null;
+
+    constructor() { }
+
+    public async init(tileWidth: number, tileHeight: number, canvas: HTMLCanvasElement, fpsCounter?: HTMLElement) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        if (!this.ctx) throw new Error("Could not get 2D context.");
+        this.ctx.imageSmoothingEnabled = false;
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+        // add click event
+        canvas.addEventListener("click", (e) => {
+            if (!this.ctx) throw new Error("Swindle not initialized.");
+            let rect = this.canvas.getBoundingClientRect();
+            let x = e.clientX - rect.left;
+            let y = e.clientY - rect.top;
+            let tileX = Math.floor(x / this.tileWidth);
+            let tileY = Math.floor(y / this.tileHeight);
+            let tile = this.tilemaps[this.tilemaps.length - 1].getTile(tileX, tileY);
+            let event = new SwindleClickEvent(x, y, tileX, tileY, tile);
+            if (this.onSwindleClick) this.onSwindleClick(event);
+        });
+        // add hover event
+        canvas.addEventListener("mousemove", (e) => {
+            if (!this.ctx) throw new Error("Swindle not initialized.");
+            let rect = this.canvas.getBoundingClientRect();
+            let x = e.clientX - rect.left;
+            let y = e.clientY - rect.top;
+            let tileX = Math.floor(x / this.tileWidth);
+            let tileY = Math.floor(y / this.tileHeight);
+            const tilemap = this.tilemaps[this.tilemaps.length - 1];
+            if (tileX >= tilemap.width || tileY >= tilemap.height || tileX < 0 || tileY < 0) return;
+            let tile = this.tilemaps[this.tilemaps.length - 1].getTile(tileX, tileY);
+            let event = new SwindleTileHoverEvent(x, y, tileX, tileY, tile);
+            if (this.onSwindleTileHover) this.onSwindleTileHover(event);
+        });
+        this.fpsCounter = fpsCounter;
+    }
+
+    public async loadSprite(name: string, path: string): Promise<HTMLImageElement> {
+        if (this.spriteMap.has(name)) return this.spriteMap.get(name);
+        let img = new Image();
+        img.src = path;
+        await img.decode();
+        this.spriteMap.set(name, img);
+        return img;
+    }
+
+    public async getSprite(name: string): Promise<HTMLImageElement> {
+        if (!this.spriteMap.has(name)) throw new Error("Sprite not loaded: " + name);
+        return this.spriteMap.get(name);
+    }
+
+    public async render(): Promise<void> {
+        if (!this.ctx) throw new Error("Swindle not initialized.");
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        for (let layer of this.tilemaps) {
+            requestAnimationFrame(() => {
+                for (let y = 0; y < layer.height; y++) {
+                    for (let x = 0; x < layer.width; x++) {
+                        let tile = layer.getTile(x, y);
+                        if (!tile) continue;
+                        if (tile.skip) continue;
+                        this.ctx.drawImage(tile.image, x * tile.width, y * tile.height);
+                    }
+                }
+            });
+        }
+        let now = performance.now();
+        let fps = 1000 / (now - this.lastFrameTime);
+        this.lastFrameTime = now;
+        if (this.fpsCounter) {
+            this.fpsCounter.innerText = fps.toFixed(2) + " FPS";
+        }
+    }
+}
