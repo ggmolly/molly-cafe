3c7a78137f9e379a0b7b6dabf4448d862f34546d
BetterCallMolly <molly@mana.rip>
2023-08-11 23:45:47 +0200

    Revert "update: wait between retries in `IdleUptime.go`"
    This reverts commit f1ec2a78ea7e076b668d0d05633cbe0511ebf9cd.

--- server/watchdogs/IdleUptime.go
+++ server/watchdogs/IdleUptime.go
@@ -13,16 +13,12 @@ func MonitorIdleUptime(packet *socket.Packet) {
 	for {
 		file, err := os.Open("/proc/uptime")
 		if err != nil {
-			log.Println("/!\\ Failed to open /proc/uptime")
-			time.Sleep(REFRESH_DELAY)
-			continue
+			log.Fatal(err)
 		}
 		var idleUptime, totalUptime float64
 		_, err = fmt.Fscanf(file, "%f %f", &totalUptime, &idleUptime)
 		if err != nil {
-			log.Println("/!\\ Failed to open /proc/uptime")
-			time.Sleep(REFRESH_DELAY)
-			continue
+			log.Fatal(err)
 		}
 		packet.SetPercentage(float32(idleUptime) / float32(totalUptime))
 		file.Close()
