f1ec2a78ea7e076b668d0d05633cbe0511ebf9cd
BetterCallMolly <molly@mana.rip>
2023-08-11 23:42:11 +0200

    update: wait between retries in `IdleUptime.go`

--- server/watchdogs/IdleUptime.go
+++ server/watchdogs/IdleUptime.go
@@ -13,12 +13,16 @@ func MonitorIdleUptime(packet *socket.Packet) {
 	for {
 		file, err := os.Open("/proc/uptime")
 		if err != nil {
-			log.Fatal(err)
+			log.Println("/!\\ Failed to open /proc/uptime")
+			time.Sleep(REFRESH_DELAY)
+			continue
 		}
 		var idleUptime, totalUptime float64
 		_, err = fmt.Fscanf(file, "%f %f", &totalUptime, &idleUptime)
 		if err != nil {
-			log.Fatal(err)
+			log.Println("/!\\ Failed to open /proc/uptime")
+			time.Sleep(REFRESH_DELAY)
+			continue
 		}
 		packet.SetPercentage(float32(idleUptime) / float32(totalUptime))
 		file.Close()
