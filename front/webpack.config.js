d9c01664917e2a71f7859a4488128b6f9c1781b0
BetterCallMolly <molly@mana.rip>
2023-08-13 16:58:10 +0200

    add: stupidly complex webpack stuff to compile into a js file

---
+++ front/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/main.ts", // Update the path to point to the src folder
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname)
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+        alias: {
+            "@src": path.resolve(__dirname, "src") // Create an alias for the src folder
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/
+    }
+};
