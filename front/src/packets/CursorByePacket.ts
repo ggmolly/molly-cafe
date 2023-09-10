68db9beb111f574bd587271341a4ea772f868dfe
BetterCallMolly <molly@mana.rip>
2023-09-10 17:36:01 +0200

    add: `CursorByePacket`

---
+++ front/src/packets/CursorByePacket.ts
@@ -0,0 +1,11 @@
+import { APacket } from "./APacket";
+
+export class CursorByePacket extends APacket {
+    update() {}
+
+    render() {}
+
+    renderOrUpdate() {
+        document.getElementById("cursor-" + this.data)!!.remove();
+    }
+}
