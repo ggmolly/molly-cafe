59aa9defd06b0529a959c46248927276735b69e2
BetterCallMolly <molly@mana.rip>
2023-09-10 17:37:16 +0200

    add: constants errors

--- server/socket/packets.go
+++ server/socket/packets.go
@@ -1,5 +1,9 @@
 package socket
 
+import "errors"
+
 var (
-	PacketMap = make(map[string]*Packet)
+	PacketMap         = make(map[string]*Packet)
+	InvalidPacketErr  = errors.New("invalid packet size")
+	ErrInvalidChannel = errors.New("invalid subscription channel")
 )
