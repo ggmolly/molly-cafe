4bc337c63c2a2ea4af7486990fe25a4dc159eae7
BetterCallMolly <molly@mana.rip>
2023-09-10 18:26:28 +0200

    add: cursor interpolation

--- front/src/packets/MouseActionPacket.ts
+++ front/src/packets/MouseActionPacket.ts
@@ -9,12 +9,33 @@ export class MouseActionPacket {
         this.clientId = data.getUint32(8);
     }
 
+    interpolate(from: number[], to: number[], steps: number): void {
+        let cursor: HTMLImageElement = document.getElementById("cursor-" + this.clientId) as HTMLImageElement;
+        if (cursor === null) {
+            throw new Error("Cursor not found");
+        }
+        let currentStep = 0;
+        let x: number, y: number;
+        const stepX = (to[0] - from[0]) / steps;
+        const stepY = (to[1] - from[1]) / steps;
+        const animate = () => {
+            x = from[0] + stepX * currentStep;
+            y = from[1] + stepY * currentStep;
+            cursor.style.left = x.toString() + "px";
+            cursor.style.top = y.toString() + "px";
+            currentStep++;
+            if (currentStep <= steps) {
+                requestAnimationFrame(animate);
+            }
+        };
+        requestAnimationFrame(animate);
+    }
+
     render() {
         let cursor: HTMLImageElement = document.createElement("img");
         cursor.src = "/assets/cursor.png";
         cursor.id = "cursor-" + this.clientId;
         cursor.style.position = "fixed";
-        // remove the scroll offset
         cursor.style.left = (this.percentageX * window.innerWidth - window.scrollX).toString() + "px";
         cursor.style.top = (this.percentageY * window.innerHeight - window.scrollY).toString() + "px";
         document.getElementById("cursors")!!.appendChild(cursor);
@@ -25,9 +46,9 @@ export class MouseActionPacket {
         if (cursor === null) {
             throw new Error("Cursor not found");
         }
-        // restore the scroll offset
-        cursor.style.left = (this.percentageX * window.innerWidth - window.scrollX).toString() + "px";
-        cursor.style.top = (this.percentageY * window.innerHeight - window.scrollY).toString() + "px";
+        let x: number = this.percentageX * window.innerWidth - window.scrollX;
+        let y: number = this.percentageY * window.innerHeight - window.scrollY;
+        this.interpolate([parseInt(cursor.style.left), parseInt(cursor.style.top)], [x, y], 10);
     }
 
     renderOrUpdate() {
