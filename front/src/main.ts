ff9aa34e34eaff4d29e31a41b0db2207cbf805c1
BetterCallMolly <molly@mana.rip>
2023-09-10 17:36:16 +0200

    update: auto-reconnecting socket

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,59 +1,4 @@
-import { MonitoringPacket } from "./packets/MonitoringPacket";
-import { PistachePacket } from "./packets/PistachePacket";
-import { SchoolProjectPacket } from "./packets/SchoolProjectPacket";
-import { StrawberryPacket } from "./packets/StrawberryPacket";
-import { StrawberrySeekPacket } from "./packets/StrawberrySeekPacket";
-import { StrawberryStatePacket } from "./packets/StrawberryStatePacket";
-
-let connectedClients: number = 0;
-
-const packetTypes: Record<number, any> = {
-    0x00: MonitoringPacket,
-    0x01: SchoolProjectPacket,
-    0x02: PistachePacket,
-    0x03: StrawberryPacket,
-    0x04: StrawberrySeekPacket,
-    0x05: StrawberryStatePacket,
-}
-
-function connectToSocket() {
-    let websocket = new WebSocket('ws://localhost:50154/ws');
-    websocket.binaryType = 'arraybuffer';
-    websocket.onmessage = (event) => {
-        const data = new DataView(event.data);
-        switch (data.getUint8(0)) {
-            case 0xFE:
-                connectedClients--;
-                document.getElementById('connected-count')!!.innerText = connectedClients.toString();
-                break;
-            case 0xFF:
-                connectedClients++;
-                document.getElementById('connected-count')!!.innerText = connectedClients.toString();
-                break;
-            case 0xFD:
-                connectedClients = data.getUint32(1);
-                document.getElementById('connected-count')!!.innerText = connectedClients.toString();
-                break;
-            case 0xFC: // DOMPopPacket
-                document.getElementById(new TextDecoder().decode(event.data.slice(1)))!!.remove();
-                break;
-            default:
-                const target: number = new Uint8Array(event.data)[0];
-                const buff: DataView = new DataView(event.data.slice(1));
-                new packetTypes[target](buff).renderOrUpdate();
-                break;
-        }
-    };
-    websocket.onclose = () => {
-        setTimeout(connectToSocket, 1000);
-    };
-    websocket.onerror = (error) => {
-        console.error(error);
-        setTimeout(connectToSocket, 1000);
-    };
-}
-
-connectToSocket();
+import { CafeSocket } from "./socket/socket";
 
 // Make the disc spin, if he has the spinning class
 document.addEventListener("readystatechange", (event: Event) => {
@@ -71,4 +16,6 @@ document.addEventListener("readystatechange", (event: Event) => {
             }
         }
     }, 10);
+
+    let socket: CafeSocket = new CafeSocket();
 });
---
+++ front/src/socket/socket.ts
@@ -0,0 +1,156 @@
+import { CursorByePacket } from "../packets/CursorByePacket";
+import { MonitoringPacket } from "../packets/MonitoringPacket";
+import { MouseActionPacket } from "../packets/MouseActionPacket";
+import { PistachePacket } from "../packets/PistachePacket";
+import { SchoolProjectPacket } from "../packets/SchoolProjectPacket";
+import { StrawberryPacket } from "../packets/StrawberryPacket";
+import { StrawberrySeekPacket } from "../packets/StrawberrySeekPacket";
+import { StrawberryStatePacket } from "../packets/StrawberryStatePacket";
+import { AAction } from "./actions/AAction";
+import { MouseMoveAction } from "./actions/MouseMoveAction";
+import { SubscribeActionTypes } from "./actions/types";
+
+const SUB_HEADER = 0x00;
+const UNSUB_HEADER = 0x01;
+const packetTypes: Record<number, any> = {
+    0x00: MonitoringPacket,
+    0x01: SchoolProjectPacket,
+    0x02: PistachePacket,
+    0x03: StrawberryPacket,
+    0x04: StrawberrySeekPacket,
+    0x05: StrawberryStatePacket,
+    0x07: CursorByePacket,
+
+    0x06: MouseActionPacket,
+};
+
+// These sessions get (re)populated when the websocket (re)connects to the server, so we need to clear them
+let RECONNECT_CLEANUP: string[] = [
+    "blog-posts",
+    "school-projects",
+    "services",
+    "hard-resources",
+    "soft-resources",
+    "misc",
+]
+
+export class CafeSocket {
+    websocket: WebSocket;
+    connectedClients: number;
+    subscriptions: Set<SubscribeActionTypes>;
+    actions: Record<SubscribeActionTypes, AAction>;
+
+    subscribe(actionType: SubscribeActionTypes) {
+        this.actions[actionType].writeLocalStorage(true);
+        this.websocket.send(new Uint8Array([SUB_HEADER, actionType]));
+        this.subscriptions.add(actionType);
+        this.actions[actionType].addEventListener();
+    }
+
+    unsubscribe(actionType: SubscribeActionTypes) {
+        this.actions[actionType].writeLocalStorage(false);
+        this.websocket.send(new Uint8Array([UNSUB_HEADER, actionType]));
+        this.subscriptions.delete(actionType);
+        this.actions[actionType].removeEventListener();
+    }
+
+    isSubscribed(actionType: SubscribeActionTypes): boolean {
+        return this.subscriptions.has(actionType);
+    }
+
+    setupSubscriptions() {
+        this.subscriptions.clear();
+        // Loop through all the actions and subscribe to them if they are in local storage
+        for (let actionType: SubscribeActionTypes = 0; actionType < Object.keys(this.actions).length; actionType++) {
+            if (this.actions[actionType].readLocalStorage()) {
+                this.subscribe(actionType);
+            }
+        }
+    }
+
+    setHandlers() {
+        this.websocket.onmessage = (event) => {
+            const data = new DataView(event.data);
+            switch (data.getUint8(0)) {
+                case 0xFE:
+                    this.connectedClients--;
+                    document.getElementById('connected-count')!!.innerText = this.connectedClients.toString();
+                    break;
+                case 0xFF:
+                    this.connectedClients++;
+                    document.getElementById('connected-count')!!.innerText = this.connectedClients.toString();
+                    break;
+                case 0xFD:
+                    this.connectedClients = data.getUint32(1);
+                    document.getElementById('connected-count')!!.innerText = this.connectedClients.toString();
+                    break;
+                case 0xFC: // DOMPopPacket
+                    document.getElementById(new TextDecoder().decode(event.data.slice(1)))!!.remove();
+                    break;
+                default:
+                    const target: number = new Uint8Array(event.data)[0];
+                    const buff: DataView = new DataView(event.data.slice(1));
+                    try {
+                        new packetTypes[target](buff).renderOrUpdate();
+                    } catch (e) {
+                        console.error("Failed to render / update packet id " + target + ", is the packet implemented?\nError: " + e);
+                    }
+                    break;
+            }
+        };
+
+        this.websocket.onclose = (event) => {
+            if (event.wasClean) {
+                console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+            } else {
+                console.log('[close] Connection died, reconnecting in 1 second');
+                setTimeout(() => {
+                    this.websocket = new WebSocket('ws://localhost:50154/ws');
+                    this.websocket.binaryType = 'arraybuffer';
+                    this.setHandlers();
+                }, 1000);
+            }
+        };
+
+        this.websocket.onopen = (error) => {
+            // Loop through all the actions and set the websocket
+            for (let actionType: SubscribeActionTypes = 0; actionType < Object.keys(this.actions).length; actionType++) {
+                this.actions[actionType].setWebsocket(this.websocket);
+                this.actions[actionType].removeEventListener(); // Remove all event listeners if the socket reconnects
+                document.getElementById(this.actions[actionType].domID!!)!!.addEventListener("change", (event) => {
+                    if ((event.target as HTMLInputElement).checked) {
+                        this.subscribe(actionType);
+                    } else {
+                        this.unsubscribe(actionType);
+                    }
+                });
+            }
+            // Clear sections that are populated by the websocket
+            for (let section of RECONNECT_CLEANUP) {
+                document.getElementById(section)!!.innerHTML = "";
+            }
+            this.setupSubscriptions();
+        };
+    }
+
+    constructor() {
+        this.websocket = new WebSocket('ws://localhost:50154/ws');
+        this.websocket.binaryType = 'arraybuffer';
+        this.connectedClients = 0;
+        this.subscriptions = new Set<SubscribeActionTypes>();
+        this.setHandlers();
+        this.actions = {
+            [SubscribeActionTypes.S_CURSOR]: new MouseMoveAction(),
+        };
+
+        // For each actions, check if the user is subscribed to it, and if so, check the checkbox
+        for (let actionType: SubscribeActionTypes = 0; actionType < Object.keys(this.actions).length; actionType++) {
+            console.log(this.actions[actionType].readLocalStorage(), document.getElementById(this.actions[actionType].domID!!), this.actions[actionType].domID)
+            if (this.actions[actionType].readLocalStorage()) {
+                document.getElementById(this.actions[actionType].domID!!)!!.setAttribute("checked", "");
+            } else {
+                document.getElementById(this.actions[actionType].domID!!)!!.removeAttribute("checked");
+            }
+        }
+    }
+}
