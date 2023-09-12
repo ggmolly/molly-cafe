5890b74d0252aff47011785a17370b667af1c7dd
BetterCallMolly <molly@mana.rip>
2023-09-12 22:56:17 +0200

    update: small bitwise hack to avoid subpixels

--- front/src/rendering/objects/bases/ADrawable.ts
+++ front/src/rendering/objects/bases/ADrawable.ts
@@ -42,7 +42,8 @@ export abstract class ADrawable {
      */
     public draw() {
         this._context.globalAlpha = this.alpha;
-        this._context.drawImage(this.sprite, this.pos.x, this.pos.y);
+        // bitwise hack to remove decimals
+        this._context.drawImage(this.sprite, this.pos.x | 0, this.pos.y | 0);
         this._context.globalAlpha = 1;
     }
 
