fc6f24d95962ed59ee8baadfe479d530d6bb701a
BetterCallMolly <molly@mana.rip>
2023-11-18 16:02:20 +0100

    mv: paths configuration to its file

--- server/configuration/RootPath.go
+++ server/configuration/RootPath.go
@@ -2,8 +2,15 @@ package configuration
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+)
+
+var (
+	ProjectPath  = "./projects"
+	PistacheRoot = "./pistache"
+	TemplateRoot = "./front"
 )
 
 // GetRootPath returns the correct path of a folder, checking cwd or parent directory (1 level)
@@ -19,4 +26,25 @@ func GetRootPath(folderName string) (string, error) {
 	}
 	// Otherwise, return an error
 	return "", fmt.Errorf("folder %s not found", folderName)
+}
+
+func init() {
+	log.Println("Loading paths...")
+	var err error
+	PistacheRoot, err = GetRootPath("pistache")
+	if err != nil {
+		log.Println("'pistache' folder could not be found. pistache will be disabled")
+	} else {
+		log.Printf("Pistache root set to %s", PistacheRoot)
+	}
+	if os.Getenv("MODE") == "dev" {
+		TemplateRoot = "../front/"
+	}
+	log.Println("Template root set to", TemplateRoot)
+	ProjectPath, err = GetRootPath("projects")
+	if err != nil {
+		log.Println("'projects' folder could not be found. project management will be disabled")
+	} else {
+		log.Printf("Monitoring projects from %s", ProjectPath)
+	}
 }
